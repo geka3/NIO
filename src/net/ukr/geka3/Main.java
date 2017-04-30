fc3a93437631b269c59d07710861a06bed109696
1 <1@geka>
2017-04-30 16:12:33 +0300

    first commit

---
+++ src/net/ukr/geka3/Main.java
@@ -0,0 +1,42 @@
+package net.ukr.geka3;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class Main {
+	//second type
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Path  p = Paths.get("pr.txt");
+//		System.out.println(p.getNameCount());
+//		System.out.println(p.getParent());
+//		System.out.println(p.isAbsolute());
+		Path kroshka = Paths.get("kroshka.txt");
+		Path p2 = Paths.get("p2.txt");
+		try {
+			Files.deleteIfExists(kroshka);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		try {
+			Files.createFile(kroshka);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ src/net/ukr/geka3/SecondClass.java
@@ -0,0 +1,11 @@
+package net.ukr.geka3;
+
+public class SecondClass {
+
+	public SecondClass() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("Class seconfClass created" );
+	}
+	
+}
